72063addb6eb1bf0dba290b84a82d811326b962d
marcusssssssssssssss <133201452+marcusssssssssssssss@users.noreply.github.com>
2023-06-01 09:09:31 +0200

    Update 1v1snakeJ1.js

--- gamejs/1v1snakeJ1.js
+++ gamejs/1v1snakeJ1.js
@@ -139,6 +139,8 @@ document.addEventListener("keydown", function(event) {
 		direction = "left";
 	} else if (event.keyCode == 87 && direction != "down") { 	//W - up
 		direction = "up";
+	} else if (event.keyCode == 32) {
+		location.reload();
 	}
 });
 
